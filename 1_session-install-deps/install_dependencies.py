8b1db3e96870dcb1f356ffbf8da6749a96b0c4f8
asong-c <mecha_alex@cloudera.com>
2023-07-19 09:54:47 -0700

    Update install_dependencies.py to enable pip logging

--- 1_session-install-deps/install_dependencies.py
+++ 1_session-install-deps/install_dependencies.py
@@ -1,1 +1,1 @@
-!pip install --no-cache-dir -r 1_session-install-deps/requirements.txt
+!pip install --no-cache-dir --log 1_session-install-deps/pip-req.log -r 1_session-install-deps/requirements.txt
